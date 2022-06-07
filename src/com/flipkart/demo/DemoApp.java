dded8ffc939d87edec507ecf143ea3ebf4a3ff23
niku22021998 <nehal.chandra@flipkart.com>
2022-06-07 08:43:37 +0530

    saving java project

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,12 @@
+package com.flipkart.demo;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		
+	}
+
+}
